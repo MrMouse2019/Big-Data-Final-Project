7ad88f49d59e30b8a8c6bfabf55116c248e1ef73
wm1065 <wm1065@nyu.edu>
2019-11-11 13:04:17 -0500

    Make for_files.sh able to run python programs

--- for_files.sh
+++ for_files.sh
@@ -5,11 +5,14 @@ module load spark/2.2.0
 
 # allow match-no-files pattern to expand to a null string
 shopt -s nullglob
+
+PROGRAM_NAME=for_files_temp.py
  
 /usr/bin/hadoop fs -find /user/hm74/NYCOpenData  > input
- 
+  
 while IFS= read -r line; do
-	echo "$line"
-done < input
- 
+	spark-submit $PROGRAM_NAME $line
+	done < input
+	 
 rm input
+
