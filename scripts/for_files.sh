ad7ff7019ed77a598e333416d38a2cb8d06c312d
MrMouse2019 <56179173+MrMouse2019@users.noreply.github.com>
2019-11-25 13:46:35 -0500

    Update for_files.sh

--- scripts/for_files.sh
+++ scripts/for_files.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 module load python/gnu/3.6.5
-module load spark/2.2.0
+module load spark/2.4.0
 
 # allow match-no-files pattern to expand to a null string
 shopt -s nullglob
 
-PROGRAM_NAME=for_files_temp.py
+PROGRAM_NAME=task1_test.py
  
 /usr/bin/hadoop fs -find /user/hm74/NYCOpenData  > input
-  
+
 while IFS= read -r line; do
-	spark-submit $PROGRAM_NAME $line
-	done < input
+	echo "Test dataset: $line ..."
+	if [ "$line" != "/user/hm74/NYCOpenData" ]; then
+		spark-submit $PROGRAM_NAME $line
+	fi
+done < input
 	 
 rm input
