adc3c326cc16dcfaf632d9fe09524c27b0b21849
wm1065 <wm1065@nyu.edu>
2019-12-01 14:46:51 -0500

    Upload results for task1

---
+++ scripts/for_mid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load python/gnu/3.6.5
+module load spark/2.4.0
+
+# allow match-no-files pattern to expand to a null string
+shopt -s nullglob
+
+/usr/bin/hadoop fs -ls /user/hm74/NYCOpenData | awk '{if ($5 >= 50000000 && $5 <= 100000000) print $8}'  > input
+
+while IFS= read -r line; do
+	if [ "$line" != "Found 1901 items" ]; then
+		echo "Test dataset: $line ..."
+		spark-submit task1_test.py $line
+	fi
+done < input
+
+rm input
