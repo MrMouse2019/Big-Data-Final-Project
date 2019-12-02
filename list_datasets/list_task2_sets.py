de07b61a8e9188357d6c121cf0b9f1cbf685c636
wm1065 <wm1065@nyu.edu>
2019-12-02 14:24:49 -0500

    Add a python script to extract dataset name/path/col names

---
+++ list_datasets/list_task2_sets.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+import sys
+
+def process(ds_name):
+	input = 'list_all_datasets.txt'
+	d_name = ds_name[1:10]
+	col = ds_name[11: -9]
+	d_path = '/user/hm74/NYCOpenData/' + d_name + '.tsv.gz'
+	print (d_name)
+	print (col)
+	print (d_path)
+	print ('')
+	with open(input, 'r') as file:
+		while True:
+			line = file.readline()
+			if not line:
+				break
+			if len(line) != 94:
+				continue
+			ds = line[-17:-8]
+			if ds == d_name:
+				#print(ds)
+				#print(ds_name)
+				#print (line)
+				pass
+	return
+
+
+filename = 'list_task2_datasets.txt'
+with open(filename, 'r') as f:
+	while True:
+		dataset_name = f.readline()
+		if not dataset_name:
+			break
+		process(dataset_name)
