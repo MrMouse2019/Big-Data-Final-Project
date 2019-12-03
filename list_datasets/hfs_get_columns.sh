0c2d7a4d12b5121d9c1f6fed83472f8f1f9701e0
wm1065 <wm1065@nyu.edu>
2019-12-03 12:27:21 -0500

    hfs_get_columns.sh

---
+++ list_datasets/hfs_get_columns.sh
@@ -0,0 +1,8 @@
+# allow match-no-files pattern to expand to a null string
+shopt -s nullglob
+
+while IFS= read -r line; do
+	echo Getting column: /user/hm74/NYCColumns/$line
+	/usr/bin/hadoop fs -get /user/hm74/NYCColumns/$line
+done < list_task2_datasets.txt
+
