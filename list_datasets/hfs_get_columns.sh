8c691ac71cd432d73e5f951efc225cc5473d13ca
wm1065 <wm1065@nyu.edu>
2019-12-03 12:48:42 -0500

    Fix minor dir name error in hfs_get_columns.sh

--- list_datasets/hfs_get_columns.sh
+++ list_datasets/hfs_get_columns.sh
@@ -6,6 +6,6 @@ while IFS= read -r line; do
 	echo Getting column: /user/hm74/NYCColumns/$line
 	/usr/bin/hadoop fs -get /user/hm74/NYCColumns/$line
 	mv $line task2_datasets
-	gunzip task2_datasets/$line
+	gunzip task2_datasets_unzip/$line
 done < list_task2_datasets.txt
 
