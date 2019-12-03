1faccd3faa2ff720638f8ad229fcae715a77c13d
wm1065 <wm1065@nyu.edu>
2019-12-03 12:28:55 -0500

    Updated hfs_get_columns.sh to put all colmns in one folder

--- list_datasets/hfs_get_columns.sh
+++ list_datasets/hfs_get_columns.sh
@@ -1,8 +1,10 @@
 # allow match-no-files pattern to expand to a null string
 shopt -s nullglob
 
+mkdir task2_datasets
 while IFS= read -r line; do
 	echo Getting column: /user/hm74/NYCColumns/$line
 	/usr/bin/hadoop fs -get /user/hm74/NYCColumns/$line
+	mv $line task2_datasets
 done < list_task2_datasets.txt
 
