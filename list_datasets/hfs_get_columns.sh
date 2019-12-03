b8381acf02165461c48a9946ba93da045fe1b306
wm1065 <wm1065@nyu.edu>
2019-12-03 12:46:40 -0500

    Modify dir name in hfs_get_columns.sh

--- list_datasets/hfs_get_columns.sh
+++ list_datasets/hfs_get_columns.sh
@@ -1,7 +1,7 @@
 # allow match-no-files pattern to expand to a null string
 shopt -s nullglob
 
-mkdir task2_datasets
+mkdir task2_datasets_unzip
 while IFS= read -r line; do
 	echo Getting column: /user/hm74/NYCColumns/$line
 	/usr/bin/hadoop fs -get /user/hm74/NYCColumns/$line
