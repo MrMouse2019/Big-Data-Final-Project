0eacc9b849ee58f559413f53bccef8f659e0c8ab
wm1065 <wm1065@nyu.edu>
2019-12-03 12:44:33 -0500

    Update hfs_get_columns.sh to unzip the files downloaded

--- list_datasets/hfs_get_columns.sh
+++ list_datasets/hfs_get_columns.sh
@@ -6,5 +6,6 @@ while IFS= read -r line; do
 	echo Getting column: /user/hm74/NYCColumns/$line
 	/usr/bin/hadoop fs -get /user/hm74/NYCColumns/$line
 	mv $line task2_datasets
+	gunzip task2_datasets/$line
 done < list_task2_datasets.txt
 
