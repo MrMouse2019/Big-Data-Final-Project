c1f11f091ffd279c0699a926aeeff7c1a7caa29e
HHKK774 <463694472@qq.com>
2019-12-08 02:22:42 -0500

    All columns of regex perfectly classified.

--- task2code/task2_hyc.py
+++ task2code/task2_hyc.py
@@ -1,7 +1,64 @@
 import re
 import csv
 
+def is_phone_num(values):
+    thre = 0.8
+    num_match = 0
+    for val in values:
+        if re.fullmatch("^\(?\d{3}\)?-? *\d{3}-? *-?\d{4}$", val[0]):
+            num_match += 1
+    match_rate = num_match / len(values)
+    if match_rate > thre:
+        return True
+    return False
+
+def is_coord(values):
+    thre = 0.9
+    num_match = 0
+    for val in values:
+        if re.match("^\(-?\d+\.\d+, *-?\d+\.\d+\)$", val[0]):
+            num_match += 1
+    match_rate = num_match / len(values)
+    if match_rate > thre:
+        return True
+    return False
+
+def is_zipcode(values):
+    thre = 0.5
+    num_match = 0
+    for val in values:
+        strs = val[0].split()
+        str = ''.join(strs)
+        if re.fullmatch("^[0-9]{5}(?:-[0-9]{4})?$", str):
+            num_match += 1
+    match_rate = num_match/len(values)
+    if match_rate > thre:
+        return True
+    return False
+
+def is_website(values):
+    thre = 0.9
+    num_match = 0
+    for val in values:
+        str = val[0].lower()
+        if not re.search('[a-zA-Z]', str):
+            continue
+        if re.search("((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)+(\/|(\/[a-z0-9#]+\/?))*", str):
+            num_match += 1
+    match_rate = num_match / len(values)
+    if match_rate > thre:
+        return True
+    return False
+
 def predict(column_file_name, values):
+    if is_zipcode(values):
+        print('Is a zip code')
+    if is_website(values):
+        print('Is a website')
+    if is_coord(values):
+        print('Is a coordinate')
+    if is_phone_num(values):
+        print('Is a phone number')
     pass
 
 def read_column_values(column_file_name):
